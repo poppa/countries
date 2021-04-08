7345e4e58c4f3b2ac8ddfb5b1ad73317210ff9b2
Pontus Östlund <ponost@roxen.com>
2021-04-08 21:34:47 +0200

    Country item component

---
+++ src/components/CountryItem.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+  import type { Country } from '../lib'
+
+  export let country: Country
+</script>
+
+<a href={`/country/${country.alpha2Code.toLowerCase()}`} class="country">
+  <div class="image">
+    <img
+      src={country.flag}
+      alt={`Flag of ${country.name}`}
+      loading="lazy"
+      width="800"
+      height="600"
+    />
+  </div>
+  <div class="contents">
+    <h2>{country.name}</h2>
+    <p>
+      <span class="label">Population:</span>
+      {country.population}
+    </p>
+    <p>
+      <span class="label">Region:</span>
+      {country.region}
+    </p>
+    <p>
+      <span class="label">Capital:</span>
+      {country.capital}
+    </p>
+  </div>
+</a>
+
+<style lang="scss">
+  a {
+    color: var(--clr-text);
+    text-decoration: none;
+  }
+
+  .country {
+    display: block;
+    position: relative;
+    overflow: hidden;
+    font-size: var(--fs-small);
+    border-radius: var(--br);
+    background-color: var(--clr-elements);
+    box-shadow: 0 0 10px -5px var(--clr-shadow);
+    transform: scale(1);
+    transition: transform 450ms cubic-bezier(0.69, -0.28, 0.27, 1.55);
+
+    &:hover {
+      transform: scale(1.1);
+    }
+
+    h2 {
+      font-size: var(--fs-normal);
+      font-weight: var(--fw-bold);
+      margin-bottom: calc(var(--padding) - 0.5em);
+    }
+  }
+
+  .image {
+    position: relative;
+    height: 15em;
+    overflow: hidden;
+    height: 13vw;
+    max-height: 305px;
+    min-height: 180px;
+
+    img {
+      object-fit: cover;
+      max-height: 100%;
+    }
+  }
+
+  .contents {
+    padding: var(--padding);
+    font-size: var(--fs-items);
+
+    p + p {
+      margin-top: 0.3em;
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,46 @@
 <script context="module" lang="ts">
-	// SSR preload
-	export const load: Load = async ({ fetch, }) => {
-		const url = `./index.json`;
-		const res = await fetch(url);
+  // SSR preload
+  export const load: Load = async ({ fetch }) => {
+    const url = `./index.json`
+    const res = await fetch(url)
 
-		try {
-			if (res.ok) {
-				return {
-					props: {
-						countries: await res.json()
-					}
-				}
-			} else {
-				throw new Error(`Bad Resquest/response ${res.status}`)
-			}
-		} catch (err: unknown) {
-			return {
-				status: res.status,
-				error: (err as Error)
-			}
-		}
-	}
+    try {
+      if (res.ok) {
+        return {
+          props: {
+            countries: await res.json()
+          }
+        }
+      } else {
+        throw new Error(`Bad Resquest/response ${res.status}`)
+      }
+    } catch (err: unknown) {
+      return {
+        status: res.status,
+        error: err as Error
+      }
+    }
+  }
 </script>
 
 <script lang="ts">
-	import type { Load } from "@sveltejs/kit"
+  import type { Load } from '@sveltejs/kit'
+  import CountryItem from '../components/CountryItem.svelte'
 
-	import type { Country } from "../lib"
-	export let countries: Country[]
+  import type { Country } from '../lib'
+  export let countries: Country[]
 </script>
 
+<div class="countries">
+  {#each countries as country (country.alpha2Code)}
+    <CountryItem {country} />
+  {/each}
+</div>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-<!-- <pre>{JSON.stringify(countries, null, 2)}</pre> -->
+<style lang="scss">
+  .countries {
+    display: grid;
+    gap: calc(var(--padding) * 2);
+    grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+  }
+</style>
